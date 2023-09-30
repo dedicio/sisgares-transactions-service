c852ca41b80b9983f592cc8105cbab49bd4888b3
Dedicio Coelho <dedicio@gmail.com>
2023-09-30 12:59:45 -0700

    feat: create dto

---
+++ internal/dto/order.go
@@ -0,0 +1,27 @@
+package dto
+
+type OrderDto struct {
+	ID            string      `json:"id"`
+	Items         []OrderItem `json:"items"`
+	Discount      float64     `json:"discount"`
+	Status        string      `json:"status"`
+	PaymentMethod string      `json:"payment_method"`
+	TotalPrice    float64     `json:"total_price"`
+}
+
+type OrderItem struct {
+	ProductID string  `json:"product_id"`
+	Quantity  int64   `json:"quantity"`
+	Price     float64 `json:"price"`
+}
+
+type OrderResponseDto struct {
+	ID            string      `json:"id"`
+	Items         []OrderItem `json:"items"`
+	Discount      float64     `json:"discount"`
+	Status        string      `json:"status"`
+	PaymentMethod string      `json:"payment_method"`
+	TotalPrice    float64     `json:"total_price"`
+	CreatedAt     string      `json:"created_at"`
+	UpdatedAt     string      `json:"updated_at"`
+}
